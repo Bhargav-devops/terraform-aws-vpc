a3253e3ee4b7c8224ea209ef12835411e688ace3
Bhargav-devops <bhargav.devopsaws@gmail.com>
2024-09-20 19:40:55 +0530

    terraform vpc

--- variables.tf
+++ variables.tf
@@ -46,3 +46,40 @@ variable "private_subnet_cidr"{
 variable "private_subnet_tags"{
     default = {}
 }
+
+variable "database_subnet_cidr"{
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidr) == 2
+        error_message = "please provide only 2 public cidr subnet"
+    }
+}
+
+variable "database_subnet_tags"{
+    default = {}
+}
+variable "nat_gateway_tags"{
+    default = {}
+}
+variable "public_route_table_tags"{
+    default = {}
+}
+variable "private_route_table_tags"{
+    default = {}
+}
+variable "database_route_table_tags"{
+    default = {}
+}
+
+variable "is_perring_required"{
+    type = bool
+    default = false
+}
+
+variable "acceptor_vpc_id"{
+    type = string
+    default = ""
+}
+variable "vpc_peering_tags"{
+    default = {}
+}
